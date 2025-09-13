84c629067641305c07ccf54bce0d2c8f33fb509f
Eb-Wan <ewanbaroneb.com@gmail.com>
2025-09-13 03:34:05 +0200

    Leger-UI V1.0.1: Fixed render function array handling

--- leger-ui/leger-js-functions.js
+++ leger-ui/leger-js-functions.js
@@ -1,15 +1,20 @@
 function render(template, params = {}) {
     if (typeof(params) != "object") throw new Error("Invalid rendering params !");
     if (Array.isArray(params)) {
+        let html = "";
         params.forEach(element => {
+            let htmlElement = template;
             for (const [key, value] of Object.entries(element)) {
-                template = template.replaceAll("$$"+key+";", value);
+                htmlElement = htmlElement.replaceAll("$$"+key+";", value);
             }
+            html += htmlElement+"\n";
         });
+        return html;
     } else {
+        let html = template;
         for (const [key, value] of Object.entries(params)) {
-            template = template.replaceAll("$$"+key+";", value);
+            html = html.replaceAll("$$"+key+";", value);
         }
+        return html;
     }
-    return template;
 }
