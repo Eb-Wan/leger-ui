ba4c1b4b9336a362d14f98800e14dcc488376656
Eb-Wan <ewanbaroneb.com@gmail.com>
2025-11-11 23:03:38 +0100

    Added LGS-parser, tested

--- leger-ui/leger-ui.js
+++ leger-ui/leger-ui.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const lgsParse = require("./lgs-parser.js");
 
 if (!process.argv[2] || !process.argv[2].includes(".lgs") || !fs.existsSync(process.argv[2])) exitError("Must be a path to an leger script file (lgs).");
 if (process.argv[3] && !fs.existsSync(process.argv[3])) exitError("Output directory doesn't exists.");
@@ -7,12 +8,20 @@ if (process.argv[3] && !fs.existsSync(process.argv[3])) exitError("Output direct
 const projectDirectory = path.dirname(process.argv[2]);
 const outputDirectory = process.argv[3] ? process.argv[3] : path.dirname(process.argv[2]);
 
-function executeScript (path) {
-    
+executeScript(process.argv[2]);
+
+/**
+ * Execute an LGS script
+ * @param {string} path - Path to the script
+ * @returns {object} - Results of the script
+ */
+
+function executeScript(path) {
+    const parsed = lgsParse(fs.readFileSync(process.argv[2], "utf-8"));
 }
 
 function exitFinished() {
-    console.log(`\nLEGER-COMPILER : Done compiling ${process.argv[2]} !`)
+    console.log(`\nLEGER-COMPILER : Done executing ${process.argv[2]} !`)
     process.exit(0);
 }
 function exitError(message, error) {
---
+++ leger-ui/lgs-parser.js
@@ -0,0 +1,43 @@
+/**
+ * Parse LGS sting into object.
+ * @param {string} lgs - LGS string to be parsed
+ * @returns {object}
+ */
+
+function parseLgs(lgs) {
+    let parsedLgs = {};
+
+    let parsingTag = false;
+
+    let propertyName = "";
+    let propertyValue = "";
+
+    for (let i = 0; i < lgs.length; i++) {
+        const char = lgs[i];
+
+        if (propertyName && !parsingTag) {
+            propertyValue += char;
+            if (propertyValue.includes(`</${propertyName}>`)) {
+                propertyValue = propertyValue.replace(`</${propertyName}>`, "");
+                parsedLgs[propertyName] = propertyValue;
+                propertyName = "";
+                propertyValue = "";
+            }
+        }
+
+        if (char == ">" && parsingTag) parsingTag = false;
+        if (parsingTag) propertyName += char;
+        if (char == "<" && !propertyName && !parsingTag) parsingTag = true;
+    }
+    
+    for (const [key, value] of Object.entries(parsedLgs)) {
+        if (key != "view" && key != "script" && key != "style") {
+            const parsed = parseLgs(value);
+            parsedLgs[key] = Object.keys(parsed).length != 0 ? parsed : value;
+        }
+    }
+
+    return parsedLgs;
+}
+
+module.exports = parseLgs;
